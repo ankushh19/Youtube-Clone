cc7a0d9e318f07274673f5a7bedd632cb02883bf
ankushh19 <ak9108824@gmail.com>
2024-05-07 01:20:10 +0530

    integration of api

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "17971fc1bamsh75dcbd25c0977fbp14a654jsna80964dfa291",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
